f63f76e592378120d27ef3ac01db1191d1007cbe
MK <doreklamster@gmail.com>
2026-03-30 12:35:08 +0200

    fix: center DMG icons and reduce window size

--- batch-uploader/scripts/fix-dmg.sh
+++ batch-uploader/scripts/fix-dmg.sh
@@ -33,12 +33,12 @@ tell application "Finder"
     set current view of container window to icon view
     set toolbar visible of container window to false
     set statusbar visible of container window to false
-    set the bounds of container window to {400, 200, 900, 470}
+    set the bounds of container window to {400, 200, 930, 510}
     set viewOptions to the icon view options of container window
     set arrangement of viewOptions to not arranged
     set icon size of viewOptions to 80
-    set position of item "Ergonode Batch Uploader.app" of container window to {140, 110}
-    set position of item "Applications" of container window to {360, 110}
+    set position of item "Ergonode Batch Uploader.app" of container window to {150, 140}
+    set position of item "Applications" of container window to {380, 140}
     close
     open
   end tell
